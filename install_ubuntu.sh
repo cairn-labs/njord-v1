c5daa61ee3dc0895cee73c741c954b837ef9e616
Sam Brotherton <sbrother@gmail.com>
2020-04-23 23:23:54 -0600

    Added ubuntu install script

---
+++ install_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+sudo add-apt-repository ppa:timescale/timescaledb-ppa
+sudo apt-get update
+sudo apt install -y timescaledb-postgresql-10
+sudo sh -c $'echo "shared_preload_libraries = \'timescaledb\'" >> /etc/postgresql/10/main/postgresql.conf'
+
+sleep 5
+sudo service postgresql restart
+sleep 5
+
+sudo -u postgres bash -c "psql -c \"CREATE USER trader_dev WITH PASSWORD 'password' SUPERUSER;\""
+sudo -u postgres bash -c "createdb trader_dev -O trader_dev"
+PGPASSWORD=password psql -h localhost -U trader_dev -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
