160a3f963fb492cff3759de37f64a0ba049a17f0
Sam Brotherton <sbrother@gmail.com>
2020-05-20 21:09:48 -0600

    Allow price only vectorization

--- analyst/vectorization/frame_vectorization.py
+++ analyst/vectorization/frame_vectorization.py
@@ -1,10 +1,12 @@
 from analyst.proto.data_frame_pb2 import DataFrame
 from analyst.proto.frame_config_pb2 import FrameConfig
+from analyst.proto.feature_config_pb2 import FeatureConfig
 from analyst.proto.frame_component_pb2 import FrameComponent
+from analyst.proto.vectorization_strategy_pb2 import FULL_ORDER_BOOK, PRICE_ONLY
 from analyst.proto.data_point_pb2 import L2_ORDER_BOOK
 from analyst.proto.label_config_pb2 import FX_RATE
 from analyst.vectorization.l2_order_book_vectorization import (
-    vectorize_l2_order_book_frame_component, L2_ORDER_BOOK_SHAPE)
+    vectorize_l2_order_book_frame_component, l2_order_book_feature_shape)
 from analyst.vectorization.fx_rate_label_vectorization import vectorize_fx_rate_label
 import numpy as np
 
@@ -24,7 +26,7 @@ def vectorize_frame(data_frame: DataFrame, frame_config: FrameConfig):
 
         num_buckets = (frame_config.frame_width_ms
                        // frame_config.feature_configs[idx].bucket_width_ms)
-        target_feature_shape, arr = vectorize_component(component, frame_config)
+        target_feature_shape, arr = vectorize_component(component, frame_config.feature_configs[idx])
         target_frame_shape = (num_buckets,) + target_feature_shape
         if arr.shape != target_frame_shape:
             print(f'Error vectorizing component {idx} of frame: expecting shape '
@@ -44,9 +46,10 @@ def vectorize_frame(data_frame: DataFrame, frame_config: FrameConfig):
     return np.asarray(component_vectors), label
 
 
-def vectorize_component(component: FrameComponent, frame_config: FrameConfig):
+def vectorize_component(component: FrameComponent, feature_config: FeatureConfig):
     if component.data_point_type == L2_ORDER_BOOK:
-        return L2_ORDER_BOOK_SHAPE, vectorize_l2_order_book_frame_component(component)
+        return (l2_order_book_feature_shape(feature_config.vectorization_strategy),
+                vectorize_l2_order_book_frame_component(component, feature_config.vectorization_strategy))
 
 
 def vectorize_label(data_frame: DataFrame, frame_config: FrameConfig):
--- analyst/vectorization/l2_order_book_vectorization.py
+++ analyst/vectorization/l2_order_book_vectorization.py
@@ -1,22 +1,35 @@
 from analyst.proto.frame_component_pb2 import FrameComponent
 from analyst.proto.l2_order_book_pb2 import L2OrderBookEntry
 from analyst.proto.data_point_pb2 import DataPoint
+from analyst.proto.vectorization_strategy_pb2 import PRICE_ONLY, FULL_ORDER_BOOK
+from analyst.price_util import get_price_from_order_book
 import numpy as np
 
 L2_ORDER_BOOK_SHAPE = (2, 50, 3)
+L2_ORDER_BOOK_PRICE_ONLY_SHAPE = (1,)
 
 
-def vectorize_l2_order_book_frame_component(component: FrameComponent):
+def l2_order_book_feature_shape(format):
+    if format == FULL_ORDER_BOOK:
+        return L2_ORDER_BOOK_SHAPE
+    elif format == PRICE_ONLY:
+        return L2_ORDER_BOOK_PRICE_ONLY_SHAPE
+
+
+def vectorize_l2_order_book_frame_component(component: FrameComponent, format):
     return np.asarray([
-        vectorize_l2_order_book(d) for d in component.data
+        vectorize_l2_order_book(d, format) for d in component.data
     ])
 
 
-def vectorize_l2_order_book(data: DataPoint):
-    return np.asarray([
-        np.asarray([order_to_vector(o) for o in data.l2_order_book.bids]),
-        np.asarray([order_to_vector(o) for o in data.l2_order_book.asks]),
-    ])
+def vectorize_l2_order_book(data: DataPoint, format):
+    if format == FULL_ORDER_BOOK:
+        return np.asarray([
+            np.asarray([order_to_vector(o) for o in data.l2_order_book.bids]),
+            np.asarray([order_to_vector(o) for o in data.l2_order_book.asks]),
+        ])
+    elif format == PRICE_ONLY:
+        return np.asarray([get_price_from_order_book(data)])
 
 
 def order_to_vector(order: L2OrderBookEntry):
