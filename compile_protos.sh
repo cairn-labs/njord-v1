8d401beb079ed925011ee0f68d191c6e53e8bdcf
Sam Brotherton <sbrother@gmail.com>
2021-02-06 10:49:12 -0700

    make compile_protos.sh work for python outside of venv

--- compile_protos.sh
+++ compile_protos.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
+set -x
 
 # Elixir
 rm -rf trader/lib/proto/*.pb.ex
 protoc -I proto --elixir_out=trader/lib/proto/ proto/*.proto
 
 # Python
+source venv/bin/activate
 rm -rf analyst/proto/*_pb2.py
 protoc --proto_path=proto/ --python_out=analyst/proto/ proto/*.proto
 # this is so ridiculous, see https://github.com/protocolbuffers/protobuf/issues/1491
