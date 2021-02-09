db749322784daf1a8e0f024e53200c467c08d0d1
Sam Brotherton <sbrother@gmail.com>
2021-02-09 15:59:39 -0700

    Fix news api data collector

--- trader/lib/trader/newsapi/newsapi_data_collector.ex
+++ trader/lib/trader/newsapi/newsapi_data_collector.ex
@@ -46,15 +46,17 @@ defmodule Trader.Newsapi.NewsapiDataCollector do
 
   defp retrieve_top_headlines(country_code) do
     api_key = Application.get_env(:trader, __MODULE__)[:api_key]
-    {:ok, %HTTPoison.Response{body: response}} = HTTPoison.get(
-      @api_base_url <> "/top-headlines",
-      [],
-      params: %{"country" => country_code, "apiKey" => api_key}
-    )
+
+    {:ok, %HTTPoison.Response{body: response}} =
+      HTTPoison.get(
+        @api_base_url <> "/top-headlines",
+        [],
+        params: %{"country" => country_code, "apiKey" => api_key}
+      )
 
     response
-    |> Jason.decode!
-    |> Map.get("articles")
+    |> Jason.decode!()
+    |> Map.get("articles", [])
     |> Enum.flat_map(fn d -> headline_article_to_proto(d, country_code) end)
     |> Enum.each(&Db.DataPoints.insert_datapoint_if_not_exists/1)
   end
@@ -63,7 +65,7 @@ defmodule Trader.Newsapi.NewsapiDataCollector do
     {:ok, published_at, 0} =
       article_json
       |> Map.get("publishedAt")
-      |> DateTime.from_iso8601
+      |> DateTime.from_iso8601()
 
     case article_json_to_proto(article_json) do
       {:ok, item} ->
@@ -71,10 +73,9 @@ defmodule Trader.Newsapi.NewsapiDataCollector do
           DataPoint.new(
             event_timestamp: published_at |> DateTime.to_unix(:microsecond),
             data_point_type: :NEWS_API_ITEM,
-            news_api_item: %NewsApiItem{item |
-                                        country: country_code,
-                                        search_type: :TOP_HEADLINES}
+            news_api_item: %NewsApiItem{item | country: country_code, search_type: :TOP_HEADLINES}
           )
+
         [proto]
 
       :error ->
@@ -87,7 +88,7 @@ defmodule Trader.Newsapi.NewsapiDataCollector do
     {:ok, published_at, 0} =
       article_json
       |> Map.get("publishedAt")
-      |> DateTime.from_iso8601
+      |> DateTime.from_iso8601()
 
     case article_json_to_proto(article_json) do
       {:ok, item} ->
@@ -95,10 +96,9 @@ defmodule Trader.Newsapi.NewsapiDataCollector do
           DataPoint.new(
             event_timestamp: published_at |> DateTime.to_unix(:microsecond),
             data_point_type: :NEWS_API_ITEM,
-            news_api_item: %NewsApiItem{item |
-                                        query: query,
-                                        search_type: :QUERY}
+            news_api_item: %NewsApiItem{item | query: query, search_type: :QUERY}
           )
+
         [proto]
 
       :error ->
@@ -107,23 +107,23 @@ defmodule Trader.Newsapi.NewsapiDataCollector do
     end
   end
 
-
-  defp article_json_to_proto(
-    %{"source" => %{"name" => source},
-      "title" => title,
-      "description" => description,
-      "url" => url,
-      "content" => content
-    }) do
-    {:ok, NewsApiItem.new(
-        source: source,
-        title: title,
-        description: description,
-        url: url,
-        content: content
-      )
-    }
+  defp article_json_to_proto(%{
+         "source" => %{"name" => source},
+         "title" => title,
+         "description" => description,
+         "url" => url,
+         "content" => content
+       }) do
+    {:ok,
+     NewsApiItem.new(
+       source: source,
+       title: title,
+       description: description,
+       url: url,
+       content: content
+     )}
   end
+
   defp article_json_to_proto(_) do
     :error
   end
@@ -131,29 +131,31 @@ defmodule Trader.Newsapi.NewsapiDataCollector do
   defp retrieve_search_results(query) do
     api_key = Application.get_env(:trader, __MODULE__)[:api_key]
     calls_per_day = Application.get_env(:trader, __MODULE__)[:max_calls_per_day]
-    window_length_ms = round(86400000 / (calls_per_day / length(@searches))) * 10
-    to = DateTime.utc_now() |> DateTime.to_iso8601
+    window_length_ms = round(86_400_000 / (calls_per_day / length(@searches))) * 10
+    to = DateTime.utc_now() |> DateTime.to_iso8601()
+
     from =
       DateTime.utc_now()
       |> DateTime.add(-window_length_ms, :millisecond)
-      |> DateTime.to_iso8601
+      |> DateTime.to_iso8601()
 
-    {:ok, %HTTPoison.Response{body: response}} = HTTPoison.get(
-      @api_base_url <> "/everything",
-      [],
-      params: %{"q" => query, "apiKey" => api_key, "from" => from, "to" => to}
-    )
+    {:ok, %HTTPoison.Response{body: response}} =
+      HTTPoison.get(
+        @api_base_url <> "/everything",
+        [],
+        params: %{"q" => query, "apiKey" => api_key, "from" => from, "to" => to}
+      )
 
     response
-    |> Jason.decode!
-    |> Map.get("articles")
+    |> Jason.decode!()
+    |> Map.get("articles", [])
     |> Enum.flat_map(fn d -> search_article_to_proto(d, query) end)
     |> Enum.each(&Db.DataPoints.insert_datapoint_if_not_exists/1)
   end
 
   defp queue_next_tick(pid) do
     calls_per_day = Application.get_env(:trader, __MODULE__)[:max_calls_per_day]
-    delay = round(86400000 / (calls_per_day / length(@searches)))
+    delay = round(86_400_000 / (calls_per_day / length(@searches)))
     spawn(fn -> Process.send_after(pid, :tick, delay) end)
   end
 end
