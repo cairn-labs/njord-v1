7f1ed4b04a5f2c18979731dfe8ee866abbfd3f1e
Sam Brotherton <sbrother@gmail.com>
2021-02-13 18:48:49 -0700

    Adjust prod.exs

--- trader/config/prod.exs
+++ trader/config/prod.exs
@@ -21,44 +21,10 @@ config :logger, level: :debug
 
 
 config :trader, Trader.Repo,
-  username: "trader_dev",
+  username: "trader_prod",
   password: System.get_env("DB_PASSWORD"),
-  database: "trader_dev",
-  hostname: "localhost",
+  database: "trader_prod",
+  hostname: "db",
   show_sensitive_data_on_connection_error: false,
   pool_size: 20,
   timeout: 600_000
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :trader, TraderWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-#         transport_options: [socket_opts: [:inet6]]
-#       ]
-#
-# The `cipher_suite` is set to `:strong` to support only the
-# latest and more secure SSL ciphers. This means old browsers
-# and clients may not be supported. You can set it to
-# `:compatible` for wider support.
-#
-# `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
-# "priv/ssl/server.key". For all supported SSL configuration
-# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-#
-# We also recommend setting `force_ssl` in your endpoint, ensuring
-# no data is ever sent via http, always redirecting to https:
-#
-#     config :trader, TraderWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
