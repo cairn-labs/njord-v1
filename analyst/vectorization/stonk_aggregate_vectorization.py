ccfe65694c59c5bc8cd1336b718439c18a12389c
Sam Brotherton <sbrother@gmail.com>
2021-02-20 21:17:45 -0700

    New PRICE_AND_VOLUME vectorization strategy

--- analyst/vectorization/stonk_aggregate_vectorization.py
+++ analyst/vectorization/stonk_aggregate_vectorization.py
@@ -1,4 +1,4 @@
-from analyst.proto.vectorization_strategy_pb2 import PRICE_ONLY
+from analyst.proto.vectorization_strategy_pb2 import PRICE_ONLY, PRICE_AND_VOLUME, VectorizationStrategy
 from analyst.proto.data_point_pb2 import DataPoint
 from analyst.proto.frame_component_pb2 import FrameComponent
 from analyst.price_util import get_price_from_stonk_aggregate
@@ -6,9 +6,13 @@ import numpy as np
 
 
 def stonk_aggregate_feature_shape(format):
-    assert format == PRICE_ONLY
-    return (1,)
-
+    if format == PRICE_ONLY:
+        return (1,)
+    elif format == PRICE_AND_VOLUME:
+        return (2,)
+    else:
+        raise NotImplementedError(f"Vectorization strategy {VectorizationStrategy.Name(format)} "
+                                  "not supported for data point type STONK_AGGREGATE.")
 
 def vectorize_stonk_aggregate_frame_component(component: FrameComponent, format):
     return np.asarray([
@@ -17,5 +21,7 @@ def vectorize_stonk_aggregate_frame_component(component: FrameComponent, format)
 
 
 def vectorize_stonk_aggregate(data: DataPoint, format):
-    assert format == PRICE_ONLY
-    return np.asarray([get_price_from_stonk_aggregate(data)])
+    if format == PRICE_ONLY:
+        return np.asarray([get_price_from_stonk_aggregate(data)])
+    elif format == PRICE_AND_VOLUME:
+        return np.asarray([get_price_from_stonk_aggregate(data), data.stonk_aggregate.volume])
